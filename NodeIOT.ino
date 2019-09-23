f794297d591f90eb3ff180fd455e1c5028af786e
aasimmd <54713635+aasimmd@users.noreply.github.com>
2019-09-23 18:46:32 +0530

    Add files via upload

---
+++ NodeIOT.ino
@@ -0,0 +1,24 @@
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "";//auth code from Blynk
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "";//ssid of router
+char pass[] = "YourPassword";//password
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
